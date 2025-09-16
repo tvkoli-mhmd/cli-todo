69b17078a66364d64bce7d077a047562cddf646f
Mohammad <tvkoli.mhmd@gmail.com>
2025-09-16 13:29:45 +0330

    cleaned up the code and implemented the id creation for each task

--- main.py
+++ main.py
@@ -5,24 +5,26 @@ with open("tasks.json") as f:
 @click.group()
 def todo():
     pass
-@click.command(help="lets you add tasks!")
+@todo.command(help="lets you add tasks!")
 @click.argument("task")
 def add(task):
     new_task = {
         "title" : task,
         "status" : "pending"
     }
+    if len(all_tasks["tasks"]) > 0:
+        new_task["id"] = all_tasks["tasks"].index(all_tasks["tasks"][-1]) + 1
+    else:
+        new_task["id"] = 0
     all_tasks['tasks'].append(new_task)
     with open("tasks.json", 'w') as f:
         json.dump(all_tasks, f)
-@click.command(help="lists all the current tasks! ")
+@todo.command(help="lists all the current tasks! ")
 def show():
     if len(all_tasks['tasks']) > 0:
         for task in all_tasks['tasks']:
-            click.echo(f"title : {task["title"]}, status : {task["status"]}")
+            click.echo(f"title : {task["title"]}, status : {task["status"]}, id : {task["id"]}")
     else:
         click.echo("No tasks right now!")
-todo.add_command(add)
-todo.add_command(show)
 if __name__ == '__main__':
     todo()
